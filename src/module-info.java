12f5fb4d1ff4243dd6068ca3ca4586f913fcf5de
tinycodecrank <tinycodecrank@gmail.com>
2022-08-16 10:46:28 +0200

    remove unused dependency

--- src/module-info.java
+++ src/module-info.java
@@ -4,7 +4,6 @@ module de.tinycodecrank.Collections
 	exports de.tinycodecrank.collections.data;
 	
 	requires transitive de.tinycodecrank.Monads;
-	requires de.tinycodecrank.Sort;
 	requires de.tinycodecrank.mathUtils;
 	requires transitive de.tinycodecrank.bounded;
 }
